8787dd3ffa84c49b605866bcddbc9abd0e37434f
tedwith418 <78437936+tedwith418@users.noreply.github.com>
2022-03-28 17:39:58 -0400

    Add files via upload

--- import_vlc_gpio.py
+++ import_vlc_gpio.py
@@ -4,10 +4,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM) #numbering scheme
 
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(27, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(23, GPIO.IN)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 #playlist of songs
@@ -17,8 +17,39 @@ playlistLength = len(plist)
 
 #play the first song
 player = vlc.MediaPlayer(plist[0])
-player.audio_set_volume(30)
+player.audio_set_volume(60)
 player.play()
+
+play = True
+
+def volume_up():
+    #raise volume
+    #time.sleep(5)
+    #if already at max volume, do nothing
+    if (player.audio_get_volume() < 100):
+        player.audio_set_volume(player.audio_get_volume() + 20)
+            
+def volume_down():
+    #lower volume
+    #time.sleep(5)
+    #if already at min volume, do nothing
+    if (player.audio_get_volume() > 0):
+        player.audio_set_volume(player.audio_get_volume() - 20)
+
+def play_pause():
+    #turn off
+    if(play):
+        player.pause()
+        play = False
+    else:
+        player.play()
+        play = True
+            
+
+GPIO.add_event_detect(22, GPIO.FALLING, callback=volume_up)
+GPIO.add_event_detect(27, GPIO.FALLING, callback=volume_down)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=play_pause)
+
 
 
 #infinite loop
@@ -27,31 +58,17 @@ while (True):
     #get integer for gesture detected
     #from cameraTest import gesture
 
-    gesture_1 = GPIO.input(17)
-    gesture_2 = GPIO.input(22)
-    gesture_3 = GPIO.input(27)
-    gesture_4 = GPIO.input(23)
+    #gesture = GPIO.input(17)
+    #gesture_2 = GPIO.input(22)
+    #gesture_3 = GPIO.input(27)
+    #gesture_4 = GPIO.input(23)
     
     
     #pause/play song
     #time.sleep(5)
-    if (gesture_1 == True):
+    """if (gesture == False):
         player.pause()
         
-    #raise volume
-    elif (gesture_2 == True):
-        #time.sleep(5)
-        #if already at max volume, do nothing
-        if (player.audio_get_volume() < 100):
-            player.audio_set_volume(player.audio_get_volume() + 20)
-            
-    #lower volume
-    elif (gesture_3 == True):
-        #time.sleep(5)
-        #if already at min volume, do nothing
-        if (player.audio_get_volume() > 0):
-            player.audio_set_volume(player.audio_get_volume() - 20)
-            
     #go to next song in playlist
     elif (gesture_4 == True):
         time.sleep(5)
@@ -72,4 +89,4 @@ while (True):
             songIndex = songIndex + 1
     
     else:
-        pass
+        pass"""
